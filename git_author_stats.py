406ea2cf83ba0b34bb827379a978fd2533745f0e
Mike Simms <msimms01@gmail.com>
2017-05-24 10:34:22 -0400

    Cleaned up variable and argument names.

--- git_author_stats.py
+++ git_author_stats.py
@@ -19,61 +19,61 @@ import sys
 import datetime
 import time
 
-authorLines = collections.Counter()
-authorExpr = re.compile(r'[a-zA-Z ]+')
-timestampExpr = re.compile(r'[\d]+-[\d]+-[\d]+\s[\d]+:[\d]+:[\d]+')
+author_lines = collections.Counter()
+author_expr = re.compile(r'[a-zA-Z ]+')
+timestamp_expr = re.compile(r'[\d]+-[\d]+-[\d]+\s[\d]+:[\d]+:[\d]+')
 
-def is_empty(sourceStr):
-	return len(sourceStr) == 0
+def is_empty(source_str):
+	return len(source_str) == 0
 
-def is_comment(sourceStr, fileExt):
-	if fileExt in ['.c', '.cpp', '.cxx', '.h', '.m', '.java']:
-		if sourceStr.find('//') == 0 or sourceStr.find('/*') == 0:
+def is_comment(source_str, file_ext):
+	if file_ext in ['.c', '.cpp', '.cxx', '.h', '.m', '.java']:
+		if source_str.find('//') == 0 or source_str.find('/*') == 0:
 			return True
-	elif fileExt in ['.py']:
-		if sourceStr.find('#') == 0:
+	elif file_ext in ['.py']:
+		if source_str.find('#') == 0:
 			return True
-	elif fileExt in ['.asm']:
-		if sourceStr.find(';') == 0:
+	elif file_ext in ['.asm']:
+		if source_str.find(';') == 0:
 			return True
 	return False
 
-def is_source_line(sourceStr, fileExt):
-	if fileExt in ['.c', '.cpp', '.cxx', '.h', '.m', '.java']:
-		if sourceStr.find(';') > 0:
+def is_source_line(source_str, file_ext):
+	if file_ext in ['.c', '.cpp', '.cxx', '.h', '.m', '.java']:
+		if source_str.find(';') > 0:
 			return True
-	elif fileExt in ['.py']:
-		if len(sourceStr) > 0:
+	elif file_ext in ['.py']:
+		if len(source_str) > 0:
 			return True
 	return False
 
 def parse_line(line):
 	try:
-		asciiLine = line.decode('ascii')
-		leftParenIndex = line.find('(')
+		ascii_line = line.decode('ascii')
+		left_paren_index = line.find('(')
 
-		if leftParenIndex >= 0:
-			subLine = asciiLine[leftParenIndex + 1:]
-			authorMatch = authorExpr.search(subLine)
-			timestampMatch = timestampExpr.search(subLine)
+		if left_paren_index >= 0:
+			sub_line = ascii_line[left_paren_index + 1:]
+			author_match = author_expr.search(sub_line)
+			timestamp_match = timestamp_expr.search(sub_line)
 
-			if authorMatch is not None and timestampMatch is not None:
-				authorStr = subLine[authorMatch.start():authorMatch.end()]
-				authorStr = authorStr.strip()
-				timestampStr = subLine[timestampMatch.start():timestampMatch.end()]
-				timestampStr = timestampStr.strip()
-				remainderStr = subLine[timestampMatch.end():]
-				sourceStrIndex = remainderStr.find(')')
-				sourceStr = remainderStr[sourceStrIndex + 1:]
-				return authorStr, timestampStr, sourceStr
+			if author_match is not None and timestamp_match is not None:
+				author_str = sub_line[author_match.start():author_match.end()]
+				author_str = author_str.strip()
+				timestamp_str = sub_line[timestamp_match.start():timestamp_match.end()]
+				timestamp_str = timestamp_str.strip()
+				remainder_str = sub_line[timestamp_match.end():]
+				source_str_index = remainder_str.find(')')
+				source_str = remainder_str[source_str_index + 1:]
+				return author_str, timestamp_str, source_str
 					
 	except UnicodeDecodeError:
 		pass
 	return "", "", ""
 
-def analyze_file(file, startTime, ignoreComments, ignoreEmpty, onlySourceLines, extensions):
-	fileName, fileExt = os.path.splitext(file)
-	if fileExt not in extensions:
+def analyze_file(file, start_time, ignore_comments, ignore_empty, only_source_lines, extensions):
+	file_name, file_ext = os.path.splitext(file)
+	if file_ext not in extensions:
 		return
 	
 	p = subprocess.Popen(["git", "blame", file], stdout = subprocess.PIPE, stderr= subprocess.PIPE)
@@ -81,43 +81,43 @@ def analyze_file(file, startTime, ignoreComments, ignoreEmpty, onlySourceLines, 
 	blameLines = blameOutput.split('\n')
 
 	for line in blameLines:
-		authorStr, timestampStr, sourceStr = parse_line(line)
+		author_str, timestamp_str, source_str = parse_line(line)
 
-		if len(authorStr) > 0 and len(timestampStr) > 0 and len(sourceStr) > 0:
-			timestamp = time.mktime(datetime.datetime.strptime(timestampStr, "%Y-%m-%d %H:%M:%S").timetuple())
-			strippedStr = sourceStr.strip()
-			if ignoreComments and is_comment(strippedStr, fileExt):
+		if len(author_str) > 0 and len(timestamp_str) > 0 and len(source_str) > 0:
+			timestamp = time.mktime(datetime.datetime.strptime(timestamp_str, "%Y-%m-%d %H:%M:%S").timetuple())
+			strippedStr = source_str.strip()
+			if ignore_comments and is_comment(strippedStr, file_ext):
 				continue
-			if ignoreEmpty and is_empty(strippedStr):
+			if ignore_empty and is_empty(strippedStr):
 				continue
-			if onlySourceLines and not is_source_line(strippedStr, fileExt):
+			if only_source_lines and not is_source_line(strippedStr, file_ext):
 				continue
-			if timestamp >= startTime:
-				authorLines[authorStr] = authorLines[authorStr] + 1
+			if timestamp >= start_time:
+				author_lines[author_str] = author_lines[author_str] + 1
 
-def analyze_repo(repo, startTime, ignoreComments, ignoreEmpty, onlySourceLines, extensions):
+def analyze_repo(repo, start_time, ignore_comments, ignore_empty, only_source_lines, extensions):
 	for root, dirs, files in os.walk(repo):
 		for file in files:
-			analyze_file(os.path.join(root, file), startTime, ignoreComments, ignoreEmpty, onlySourceLines, extensions)
+			analyze_file(os.path.join(root, file), start_time, ignore_comments, ignore_empty, only_source_lines, extensions)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--repo", required=True)
-parser.add_argument("--startTime", required=False)
-parser.add_argument("--ignoreComments", default=True, required=False)
-parser.add_argument("--ignoreEmpty", default=True, required=False)
-parser.add_argument("--onlySourceLines", default=True, required=False)
+parser.add_argument("--start-time", required=False)
+parser.add_argument("--ignore-comments", default=True, required=False)
+parser.add_argument("--ignore-empty", default=True, required=False)
+parser.add_argument("--only-source-lines", default=True, required=False)
 parser.add_argument("--extensions", default=".c,.cpp,.h,.m,.py", required=False)
 args = parser.parse_args()
 
-startTime = 0
+start_time = 0
 extensions = args.extensions.split(',')
 
-if args.startTime is not None:
-	startTime = time.mktime(datetime.datetime.strptime(args.startTime, "%Y-%m-%d").timetuple())
+if args.start_time is not None:
+	start_time = time.mktime(datetime.datetime.strptime(args.start_time, "%Y-%m-%d").timetuple())
 
-fullPath = os.path.realpath(args.repo)
-os.chdir(fullPath)
-analyze_repo(fullPath, startTime, args.ignoreComments, args.ignoreEmpty, args.onlySourceLines, extensions)
+full_path = os.path.realpath(args.repo)
+os.chdir(full_path)
+analyze_repo(full_path, start_time, args.ignore_comments, args.ignore_empty, args.only_source_lines, extensions)
 
-for author in authorLines:
-	print author + "\t" + str(authorLines[author])
+for author in author_lines:
+	print author + "\t" + str(author_lines[author])
