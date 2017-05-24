a33c740fb7e89e2dbd554019294c114e75f72b6e
Mike Simms <msimms01@gmail.com>
2017-05-24 11:36:39 -0400

    Added an option to specify the end time.

--- git_author_stats.py
+++ git_author_stats.py
@@ -71,7 +71,7 @@ def parse_line(line):
 		pass
 	return "", "", ""
 
-def analyze_file(file, start_time, ignore_comments, ignore_empty, only_source_lines, extensions):
+def analyze_file(file, start_time, end_time, ignore_comments, ignore_empty, only_source_lines, extensions):
 	file_name, file_ext = os.path.splitext(file)
 	if file_ext not in extensions:
 		return
@@ -92,17 +92,18 @@ def analyze_file(file, start_time, ignore_comments, ignore_empty, only_source_li
 				continue
 			if only_source_lines and not is_source_line(strippedStr, file_ext):
 				continue
-			if timestamp >= start_time:
+			if timestamp >= start_time and timestamp < end_time:
 				author_lines[author_str] = author_lines[author_str] + 1
 
-def analyze_repo(repo, start_time, ignore_comments, ignore_empty, only_source_lines, extensions):
+def analyze_repo(repo, start_time, end_time, ignore_comments, ignore_empty, only_source_lines, extensions):
 	for root, dirs, files in os.walk(repo):
 		for file in files:
-			analyze_file(os.path.join(root, file), start_time, ignore_comments, ignore_empty, only_source_lines, extensions)
+			analyze_file(os.path.join(root, file), start_time, end_time, ignore_comments, ignore_empty, only_source_lines, extensions)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--repo", required=True, help="Path of the local repo to examine. ex: --repo /src/my_repo/")
 parser.add_argument("--start-time", required=False, help="Filter out lines that were modified before this time. ex: --repo start-time 2017-01-01")
+parser.add_argument("--end-time", required=False, help="Filter out lines that were modified after (or at) this time. ex: --repo end-time 2018-01-01")
 parser.add_argument("--ignore-comments", default=True, required=False, help="Filter out lines that start with a comment.")
 parser.add_argument("--ignore-empty", default=True, required=False, help="Filter out lines that only contain whitespace.")
 parser.add_argument("--only-source-lines", default=True, required=False, help="Only consider lines that appear to be source code lines, such as those that contain a semicolon in C-based languages.")
@@ -110,14 +111,17 @@ parser.add_argument("--extensions", default=".c,.cpp,.h,.m,.py", required=False,
 args = parser.parse_args()
 
 start_time = 0
+end_time = sys.maxint
 extensions = args.extensions.split(',')
 
 if args.start_time is not None:
 	start_time = time.mktime(datetime.datetime.strptime(args.start_time, "%Y-%m-%d").timetuple())
+if args.end_time is not None:
+	end_time = time.mktime(datetime.datetime.strptime(args.end_time, "%Y-%m-%d").timetuple())
 
 full_path = os.path.realpath(args.repo)
 os.chdir(full_path)
-analyze_repo(full_path, start_time, args.ignore_comments, args.ignore_empty, args.only_source_lines, extensions)
+analyze_repo(full_path, start_time, end_time, args.ignore_comments, args.ignore_empty, args.only_source_lines, extensions)
 
 total = 0
 for author in author_lines:
